7ae946ae09dec1bcb28abaa5d9513bd9eca6a565
xie <xyq135@gmail.com>
2022-01-03 14:26:57 +0800

    更新index.js文件引入路径

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
 // 加载个模块的内容
 // 顶部快捷
-$('.shortcut .w').load('../components/shortcut.html');
+$('.shortcut .w').load('./components/shortcut.html');
 
 // 头部
-$('.header .w').load('../components/head.html');
+$('.header .w').load('./components/head.html');
 
 // 轮播左侧导航
-$('.fs-1').load('../components/menu.html');
+$('.fs-1').load('./components/menu.html');
 
 // 第一个轮播图
-$('.fs-2 .sliderWrapper').load('../components/sliderWrapper.html');
+$('.fs-2 .sliderWrapper').load('./components/sliderWrapper.html');
 
 // 第二个轮播图
-$('.fs-2 .sliderBanner').load('../components/sliderBanner.html');
+$('.fs-2 .sliderBanner').load('./components/sliderBanner.html');
 
 // 右侧的登录
-$('.fs-3 .login').load('../components/login.html');
+$('.fs-3 .login').load('./components/login.html');
 
 // 新闻
-$('.fs-3 .news').load('../components/news.html');
+$('.fs-3 .news').load('./components/news.html');
 
 // 服务
-$('.fs-3 .service').load('../components/serves.html');
+$('.fs-3 .service').load('./components/serves.html');
 
 // 京东秒杀
-$('.seckill').load('../components/seckill.html');
+$('.seckill').load('./components/seckill.html');
 
 
 //侧边导航栏
-$('.bottom').load('../components/bottom.html')
+$('.bottom').load('./components/bottom.html')
